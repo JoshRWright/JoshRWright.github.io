eba1105831f9de8a7d9eadde30a1388314a4ce9a
JoshRWright <joshrwright521@gmail.com>
2019-12-09 18:47:48 +1100

    looking better

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 document.getElementById("app").innerHTML=new Date().toDateString()
+var myitem=document.createElement("li")
+myitem.textcontent="xyz"
+getElementById("items").getChildren().append(myitem);
